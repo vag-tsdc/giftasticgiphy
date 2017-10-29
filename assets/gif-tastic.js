e800de00df41ad4ec7ae82180d2c2098614c38db
vag-tsdc <30638922+vag-tsdc@users.noreply.github.com>
2017-10-28 23:28:43 -0400

    ALL DONE!

--- assets/gif-tastic.js
+++ assets/gif-tastic.js
@@ -10,10 +10,16 @@
 // a loop that appends a button generated buttons from topics ^
 $(document).ready(function () {
 
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            e.preventDefault();
+        }
+    });
+
 
     var thingsList = ["okay", "scared", "rihanna", "kandi"];
     for (i = 0; i < thingsList.length; i++) {
-        var button = $("<button type='button' class='btn btn-primary'>");
+        var button = $("<button type='button' class='btn btn-primary' style='margin:3px'>");
         var newButton = button.attr("value", thingsList[i]);
         newButton.html(thingsList[i]);
         $("#buttons-go-here").append(newButton);
@@ -28,75 +34,68 @@ $(document).ready(function () {
         var searchGif = $(this).val();
         console.log(searchGif);
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-        searchGif + "&api_key=67sPBFexKhzWChD2zLrs9HJI1ciqE6ta";
+            searchGif + "&api_key=67sPBFexKhzWChD2zLrs9HJI1ciqE6ta";
 
         $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function (response) {
+                url: queryURL,
+                method: "GET"
+            })
+            .done(function (response) {
 
-        // $("#gifs-appear-here").empty();
+                // $("#gifs-appear-here").empty();
+                for (var i = 0; i < 25; i++) {
+                    var results = response.data[i];
 
-            console.log(response);
+                    var gifDiv = $("<div class='item'>");
 
-            var results = response.data;
+                    var rating = results.rating;
+                    var p = $("<p>").text("Rating: " + rating);
 
-            for (var i = 0; i < results.length; i++) {
+                    var stillGif = results.images.fixed_width_still.url;
 
-                var gifDiv = $("<div class='item'>");
+                    var gif = $("<img id='thumbnail'>");
+                    gif.attr("src", stillGif);
+                    gif.attr("data-state", "still");
+                    gif.attr("animated", results.images.fixed_width.url.toString());
+                    gif.attr("still", stillGif);
 
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
+                    gifDiv.append(p);
+                    gifDiv.append(gif);
 
-                var stillGif = results[i].images.fixed_width_still.url;
+                    $("#gifs-appear-here").prepend(gifDiv);
 
-                var gif = $("<img>");
-                gif.attr("src", stillGif);
-                gif.attr("data-state", "still");
-                gif.attr("animated", results[i].images.fixed_width.url.toString());
-                gif.attr("still", stillGif);
-
-                gifDiv.append(p);
-                gifDiv.append(gif);
-
-                $("#gifs-appear-here").prepend(gifDiv);
-            }
-        });
+                }
+                $("#gif-text").html("<h3> You chose... <strong>" + searchGif + "</strong></h3>");
+            });
     });
 
 
     // add form to page that takes valye from a user input box and adds it into your topics array....
-    $("#send-it").on("click", function (event) {
-        event.preventDefault();
-        var inputButton = $("#new-button-input").val().trim();
+    $(".submit").on("click", function () {
 
-        console.log(inputButton);
+        var button = $("<button type='button' class='btn btn-primary' style='margin:3px;'></button>");
+        var searchTerm = $("#newButton").val();
+        button.html(searchTerm);
+        button.attr("value", searchTerm.toString());
+        $("#buttons-go-here").append(button);
 
-        thingsList.push(inputButton);
-
-        console.log(thingsList);
     });
-    // a function call that takes each topic and remakes the button on the page.
 
-    // $("button").on("click", function () {
+    $("#makeAButton").submit(function (e) {
+        loadAjax();
+        e.returnValue = false;
+    });
 
-    // });
-
-
-
-
-
-
-    // user clicks button:
-    // function = 
-    // grab 10 STATIC, non-animated gif images from GIPHY and place them on the page
-
-    // user clicks gif image - 
-    // animate image
-    // user clicks animated gif  - gif stops moving
-
-    // every gif - 
-    // display rating
+    $("#gifs-appear-here").on("click", "#thumbnail", function () {
+        var state = $(this).attr("data-state");
+        if (state == "still") {
+            $(this).attr("data-state", "animated");
+            $(this).attr("src", $(this).attr("animated"));
+        } else if (state == "animated") {
+            $(this).attr("data-state", "still");
+            $(this).attr("src", $(this).attr("still"));
+        }
+    });
 
 
 
